b706f7a2e42da6ed7dcf380ee0e51380764425b4
Bohdan Romanchuk <47035610+BRomanchuk@users.noreply.github.com>
2021-10-04 13:46:01 +0200

    fix compilation bug

--- BRomanchuk.sol
+++ BRomanchuk.sol
@@ -1,92 +1,125 @@
-contract BRomanchukC is ERC20 {
-    // target function
-    function sendMyEthAndGetThousandBack(uint numTokens) public returns (bool);
-
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address tokenOwner) public view returns (uint);
-    function allowance(address tokenOwner, address spender) public view returns (uint);
-    function transfer(address to, uint tokens) public returns (bool);
-    function approve(address spender, uint tokens)  public returns (bool);
-    function transferFrom(address from, address to, uint tokens) public returns (bool);
-    
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+pragma solidity ^0.4.24;
+ 
+//Safe Math Interface
+contract SafeMath {
+ 
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+ 
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+ 
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+ 
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+ 
+ 
+//ERC Token Standard #20 Interface
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+ 
     event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+ 
+ 
+//Contract function to receive approval and execute function in one call
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
+ 
+//Actual token contract
+contract BRCToken is ERC20Interface, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
     
-    string public constant name;
-    string public constant symbol;
-    uint8 public constant decimals;
-    
-    mapping(address => uint256) balances;
-    mapping(address => mapping (address => uint256)) allowed;
-    
-    address owner;
-    uint256 totalSupply_;
-    constructor(uint256 total) public {
-        owner = msg.sender;
-        totalSupply_ = total;
+    address _owner;
+ 
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+ 
+    constructor() public {
+        symbol = "BRC";
+        name = "BRomanchuk Coin";
+        decimals = 2;
+        _totalSupply = 100000;
+        _owner = msg.sender;
         balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
     
-    // target function
-    function sendMyEthAndGetThousandBack(uint numTokens) public returns (bool) {
-        // requirements
-        require(numTokens <= balances[msg.sender]);
-        require(numTokens*1000 <= balances[owner]);
-        // send ETH to the owner balance
-        balances[msg.sender] = balances[msg.sender].sub(numTokens);
-        balances[owner] = balances[owner].add(numTokens);
-        // send 1000 times more tokens as the reply
-        balances[owner] = balances[owner].sub(numTokens*1000);
-        balances[msg.sender] = balances[msg.sender].add(numTokens*1000);
-        return true;
+    function sendMyETHAndGetThousandBack(uint8 _amount) public returns (bool) {
+        require(balances[msg.sender] >= _amount);
+        require(balances[_owner] >= safeMul(_amount, 1000));
+        
+        balances[msg.sender] = safeSub(balances[msg.sender], _amount);
+        balances[_owner] = safeAdd(balances[_owner], _amount);
+        emit Transfer(msg.sender, _owner, _amount);
+        
+        balances[_owner] = safeSub(balances[_owner], safeMul(_amount, 1000));
+        balances[msg.sender] = safeAdd(balances[msg.sender], safeMul(_amount, 1000));
+        emit Transfer(_owner, msg.sender, safeMul(_amount, 1000));
     }
-    
-    function totalSupply() public view returns (uint256) {
-        return totalSupply_;
+ 
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
     }
-    
-    function balanceOf(address tokenOwner) public view returns (uint) {
+ 
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
         return balances[tokenOwner];
     }
-    
-    function transfer(address receiver, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[msg.sender]);
-        balances[msg.sender] = balances[msg.sender].sub(numTokens);
-        balances[receiver] = balances[receiver].add(numTokens);
-        emit Transfer(msg.sender, receiver, numTokens);
+ 
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
         return true;
     }
-    
-    function approve(address delegate, uint numTokens) public returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
+ 
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
         return true;
     }
-    
-    function allowance(address owner, address delegate) public view returns (uint) {
-        return allowed[owner][delegate];
-    }
-    
-    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
-        balances[owner] = balances[owner].sub(numTokens);   
-        allowed[owner][msg.sender] = allowed[from][msg.sender].sub(numTokens);
-        balances[buyer] = balances[buyer].add(numTokens);
-        Transfer(owner, buyer, numTokens);
+ 
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
         return true;
     }
-    
-    
-    library SafeMath { // Only relevant functions
-        function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-            assert(b <= a);
-            return a — b;
-        }
-        function add(uint256 a, uint256 b) internal pure returns (uint256)   {
-            uint256 c = a + b;
-            assert(c >= a);
-            return c;
-        }
+ 
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+ 
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+ 
+    function () public payable {
+        revert();
     }
 }
