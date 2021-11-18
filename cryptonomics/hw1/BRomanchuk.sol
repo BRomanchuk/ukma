01c75b2199ccb7fe5e5792416f78d664df50d5f4
Bohdan Romanchuk <47035610+BRomanchuk@users.noreply.github.com>
2021-11-18 21:07:15 +0200

    upd hw-1

--- cryptonomics/hw1/BRomanchuk.sol
+++ cryptonomics/hw1/BRomanchuk.sol
@@ -1,5 +1,10 @@
-pragma solidity ^0.4.24;
- 
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 < 0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.2/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.2/contracts/access/Ownable.sol";
+
 //Safe Math Interface
 contract SafeMath {
  
@@ -24,103 +29,31 @@ contract SafeMath {
     }
 }
  
- 
-//ERC Token Standard #20 Interface
-contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
- 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
- 
- 
-//Contract function to receive approval and execute function in one call
-contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
-}
- 
-//Actual token contract
-contract BRCToken is ERC20Interface, SafeMath {
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint public _totalSupply;
+
+contract BRCToken is Ownable, ERC20, SafeMath {
     
-    address _owner;
- 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
- 
-    constructor() public {
-        symbol = "BRC";
-        name = "BRomanchuk Coin";
-        decimals = 2;
-        _totalSupply = 100000;
-        _owner = msg.sender;
-        balances[msg.sender] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
+    uint private constant _maxSupply = 1000000;
+                                        
+    constructor() ERC20("BRCToken", "BRC") {
     }
     
-    function sendMyETHAndGetThousandBack(uint8 _amount) public returns (bool) {
-        require(balances[msg.sender] >= _amount);
-        require(balances[_owner] >= safeMul(_amount, 100));
+    function destroy() public onlyOwner{
+        selfdestruct(payable(owner()));
+    }
+    
+    receive() external payable {
+        require(msg.value > 0, "No ETH");
+        uint avaliableSupply = _maxSupply - totalSupply();
         
-        balances[msg.sender] = safeSub(balances[msg.sender], _amount);
-        balances[_owner] = safeAdd(balances[_owner], _amount);
-        emit Transfer(msg.sender, _owner, _amount);
+        uint requestedSupply = safeMul(msg.value, 100);
         
-        balances[_owner] = safeSub(balances[_owner], safeMul(_amount, 100));
-        balances[msg.sender] = safeAdd(balances[msg.sender], safeMul(_amount, 100));
-        emit Transfer(_owner, msg.sender, safeMul(_amount, 100));
-        return true;
-    }
- 
-    function totalSupply() public constant returns (uint) {
-        return _totalSupply  - balances[address(0)];
-    }
- 
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
-        return balances[tokenOwner];
-    }
- 
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
- 
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
- 
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = safeSub(balances[from], tokens);
-        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(from, to, tokens);
-        return true;
-    }
- 
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
- 
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
-        return true;
-    }
- 
-    function () public payable {
-        revert();
+        if (avaliableSupply >= requestedSupply) {
+            payable(owner()).transfer(msg.value);
+        } else {
+            uint ethForOwner = safeDiv(avaliableSupply, 100);
+            payable(owner()).transfer(ethForOwner);
+            _mint(msg.sender, avaliableSupply);
+            payable(msg.sender).transfer(safeSub(msg.value, ethForOwner));
+        }
     }
 }
